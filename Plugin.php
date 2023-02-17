f3439982c3f7bba3919b658d05193b57beec746f
NielsvandenDries <niels@openmindedit.nl>
2023-02-17 13:36:22 +0100

    Update Plugin details

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,18 @@ use System\Classes\PluginBase;
 class Plugin extends PluginBase
 {
     public $require = ['Inetis.ListSwitch'];
-    
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'Development Toolkit',
+            'description' => 'This plugin is made by NvandenDries.nl.\nIt is a very simple tracker for Projects, Roadmaps and Bugs.',
+            'author' => 'Niels van den Dries',
+            'icon' => 'oc-icon-gears',
+            'homepage' => 'https://nvandendries.nl'
+        ];
+    }
+
     public function registerComponents()
     {
     }
